5de24492ab804b0e9621cacc1b1be90d1b9db048
Giselle <gisellec60@gmail.com>
2023-07-25 13:04:33 -0400

    test passed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email = email_string
+        
+    def parse(self):
+        email_regex = re.compile(r"[\w\.]+@[\w]+\.com")
+        emails = email_regex.findall(self.email)
+        emails.sort()
+        print(emails)
+        return emails
+
